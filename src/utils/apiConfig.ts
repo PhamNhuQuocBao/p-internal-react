674ec1a81998c8c123816c0c8bb9600860b6e536
PhamNhuQuocBao <phamnhuquocbao@gmail.com>
2023-08-31 17:29:28 +0700

    mockAPI

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL_PRODUCT = "http://localhost:3000";
+const BASE_URL_PRODUCT = "https://64f068c58a8b66ecf7799be5.mockapi.io";
 
 export const API = axios.create({
   baseURL: BASE_URL_PRODUCT,
---
+++ src/views/pages/detail/index.tsx
@@ -0,0 +1,14 @@
+import { FC, memo } from "react";
+import { DataType } from "../../../interfaces/table";
+
+interface DetailPageProps {
+    product: DataType;
+}
+
+const DetailPage: FC = () => {
+  return <>
+    <main className="container"></main>
+  </>; 
+};
+
+export default memo(DetailPage);
